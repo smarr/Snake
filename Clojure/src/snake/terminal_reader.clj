664caa79f366bba62d49e840a343a953e6f4c973
Stefan Marr <git@stefan-marr.de>
2011-02-19 10:57:31 +0100

    Fixed indentation and moved recur into tail position
    - also uses case now instead of if
    - uses unicode escape sequence for character 27 (ESC)
    - not yet functional, I think

--- Clojure/src/snake/terminal_reader.clj
+++ Clojure/src/snake/terminal_reader.clj
@@ -1,24 +1,16 @@
 (ns snake.terminal-reader)
 
 (defn read-input [input-receiver]
-	(loop [data (read)]
-    (if (= data "[A")
-        ((print "UP")
-         (recur (read)))
-   ((if (= data "[B")
-        ((print "DOWN")
-         (recur (read)))
-   ( ;default
-    (recur (str data (read)))))))))
-;		(case data
-;			"[A" ( ;(send input-receiver move 'up)
-;				       (print "UP")
-;				       ; (recur [data (read)])
-;             )
-;			"[B" (print "DOWN")
-;
-;			((print (str "DEFAULT" data))
-;			 (recur (str data (read)))))))
+  (loop [data (read)]
+    (case data
+      "\u001B[A" ( ;(send input-receiver move 'up)
+              (print "UP")
+              ; (recur [data (read)])
+              )
+      "\u001B[B" (print "DOWN")
+      
+      (print (str "DEFAULT" data)))
+    (recur (str data (read)))))
 
 (defn start [input-receiver]
-	(send (agent nil) read-input input-receiver))
+  (send (agent nil) read-input input-receiver))
