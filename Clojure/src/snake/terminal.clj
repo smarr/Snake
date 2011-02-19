0ca978ec32fbcab3c7263fe1f907fb660f2d905c
Stefan Marr <git@stefan-marr.de>
2011-02-19 10:55:50 +0100

    Added documentation to terminal module

--- Clojure/src/snake/terminal.clj
+++ Clojure/src/snake/terminal.clj
@@ -1,10 +1,16 @@
 (ns snake.terminal)
 
-(defn clean []
-	(print "[2J"))
+(defn clean
+  "Clear the terminal, i.e., remove all output."
+  []
+	(print "\u001B[2J"))
 
-(defn set-cursor [x y]
-	(printf "[%d;%dH" y x))
+(defn set-cursor
+  "Set the cursor for the next output to the given coordinates."
+  [x y]
+	(printf "\u001B[%d;%dH" y x))
 
-(defn put [str]
+(defn put
+  "Output the string at the current cursor coordinates."
+  [str]
 	(print str))
