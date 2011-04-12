34e7a58fe7c5224904e48e9294eb2dcf5b971b35
Stefan Marr <git@stefan-marr.de>
2011-04-12 22:50:39 +0200

    Execute the code as a script
    - should make things easier, and was the simplest way to get it running
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- Clojure/run.sh
+++ Clojure/run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 stty -echo -echok -icanon min 1 time 0
-java -cp snake.jar:/opt/local/share/java/clojure/lib/clojure.jar snake.core
+java -cp clojure.jar:clojure-contrib.jar:src/ clojure.main src/snake/core.clj
+#java -cp snake.jar:/opt/local/share/java/clojure/lib/clojure.jar snake.core
+#lein run
 stty echo echok icanon
