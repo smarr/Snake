287cc43ac236adfebfb3c43dc42d98f75d577e4d
Stefan Marr <git@stefan-marr.de>
2011-04-12 22:53:51 +0200

    Read input directly from the System/in
    - the (read) function did not work for me, using System/in allows me to get the input to interpret cursor keys like done in the Java Snake
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- Clojure/src/snake/terminal_reader.clj
+++ Clojure/src/snake/terminal_reader.clj
@@ -1,16 +1,38 @@
 (ns snake.terminal-reader)
 
-(defn read-input [input-receiver]
-  (loop [data (read)]
-    (case data
-      "\u001B[A" ( ;(send input-receiver move 'up)
-              (print "UP")
-              ; (recur [data (read)])
-              )
-      "\u001B[B" (print "DOWN")
-      
-      (print (str "DEFAULT" data)))
-    (recur (str data (read)))))
+(defn get-input
+  "Reads a string from the given input-reader if there is anything to be read"
+  [input-reader]
+  
+  (def buffer (char-array 1024))
+  (if (.ready input-reader)
+    (do
+      (def num-chars-read (.read input-reader buffer))
+      (String/valueOf buffer 0 num-chars-read))
+    ""))
 
-(defn start [input-receiver]
-  (send (agent nil) read-input input-receiver))
+
+(defn read-input
+  "Read from standard input and decode the input into symbols
+   to be understoud be the input-receiver."
+  [input-receiver input-reader]
+  (loop [data (get-input input-reader)]
+    (case data
+      "\u001B[A" ;(send input-receiver move 'up)
+              (do
+                (print "UP")
+                (flush))
+              ; (recur [data (read)])
+              
+      "\u001B[B" (print "DOWN")
+      "\u001B[D" (print "LEFT")
+      "\u001B[C" (print "RIGHT")
+      :nothing)
+    (recur (get-input input-reader))))
+
+(defn start
+  "Starts a new thread that is reading from standard in and will
+   notify an agent about all read input."
+  [input-receiver]
+  (def input-reader (new java.io.InputStreamReader System/in))
+  (.start (Thread. (fn [] (read-input input-receiver input-reader)))))
