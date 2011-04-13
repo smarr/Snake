bae6c1054124d7e99626a85b5e912bc381fa34ad
Stefan Marr <git@stefan-marr.de>
2011-04-13 13:54:32 +0200

    The board-view is now an agent
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- Clojure/src/snake/board_view.clj
+++ Clojure/src/snake/board_view.clj
@@ -1,47 +1,55 @@
 (ns snake.board-view
   (:use snake.terminal)
+  (:use snake.agents-helpers)
   (:require [clojure.contrib.string :as string]))
 
 (defn create-view
   "Create a new board view"
-  [board]
-  :not-yet-implemented)
+  [{width :width height :height}]
+  (start-agent-and-initialize {:width  width
+                               :height height}))
 
 (defn draw-borders
   "Draws the surrounding border of the board."
-  [{width :width height :height}]
+  [board-view]
+  (let [{width  :width 
+         height :height} board-view]
+    (println width)
+    (println height)
+    ; (println board-view)
+    (set-cursor 0 0)
+    (put "/")
+    (put (string/repeat width "-"))
+    (put "\\")
   
-  (set-cursor 0 0)
-  (put "/")
-  (put (string/repeat width "-"))
-  (put "\\")
+    (dotimes [i height]
+      (set-cursor 0           (+ i 1))
+      (put "|")
+      (set-cursor (+ width 1) (+ i 1))
+      (put "|"))
   
-  (dotimes [i height]
-    (set-cursor 0           (+ i 1))
-    (put "|")
-    (set-cursor (+ width 1) (+ i 1))
-    (put "|"))
+    (put "\n")
   
-  (put "\n")
-  
-  (put "\\")
-  (put (string/repeat width "-"))
-  (put "/"))
+    (put "\\")
+    (put (string/repeat width "-"))
+    (put "/"))
+  (flush)
+  (println "down with borders"))
 
 (defmulti add-element :type)
   (defmethod add-element :apple
-    [{x :x y :y}]
+    [board-view {x :x y :y}]
     
     (set-cursor (+ x 1) (+ y 1))
     (put "o"))
   (defmethod add-element :snake
-    [{x :x y :y}]
+    [board-view {x :x y :y}]
     
     (set-cursor (+ x 1) (+ y 1))
     (put "#"))
 
 (defn remove-element
-  [{x :x y :y}]
+  [board-view {x :x y :y}]
   
   (set-cursor (+ x 1) (+ y 1))
   (put " "))
