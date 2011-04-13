ca22dd7bc16beee38c57e169c0c225e36f0ae6e2
Stefan Marr <git@stefan-marr.de>
2011-04-13 22:47:51 +0200

    Core script setting up the overall system of agents
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- Clojure/src/snake/core.clj
+++ Clojure/src/snake/core.clj
@@ -1,16 +1,33 @@
 (ns snake.core
-  (:use snake.terminal)
+  (:use snake.input-receiver)
   (:use snake.terminal-reader)
+  (:use snake.board)
+  (:use snake.player)
   (:use snake.board-view)
-	(:gen-class))
+  (:use snake.game-elements)
+  (:use snake.game-master))
 
-(defn -main [& args]
-	(snake.terminal-reader/start nil)
-	(println "Hello World!")
-	(clean)
-	(set-cursor 10 3)
-	(put "foo1")
-	(set-cursor 3 10)
-	(put "bar2")
-	(println "Hello World!2")
-	(. Thread sleep 5000))
+
+
+(def human-player       (create-human-player))
+(def board              (create-board :width  10
+                                      :height 10))
+(def board-view         (create-view  board :x 4 :y 4))
+
+(start-terminal-reader  human-player)
+
+; initialize the view
+(send  board-view  initialize-view)
+(send  board-view  draw-borders)
+
+; now start the actual game
+(def game-over    (exec-game-master board-view
+                                    board
+                                    :num-apples 5
+                                    :players   [human-player]))
+
+(println "waiting for game-over")(flush)
+
+(deref game-over) ;; will block until the games is completed
+
+(println "GAME OVER")
